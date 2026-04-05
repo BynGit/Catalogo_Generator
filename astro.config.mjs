8055b48bf0582ea8138b61dae012e9fdd6a24138
Brayan Cardona <brayancardona3778@gmail.com>
2026-04-04 23:26:11 -0500

    Fix Deployment Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 
     output: 'server', 
-    devToolbar: { enabled: false }
+    devToolbar: { enabled: false },
+    adapter: vercel(),
 
 
 });
